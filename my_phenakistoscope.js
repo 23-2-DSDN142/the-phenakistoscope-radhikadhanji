319f7ebe9e8e1d356679bef6a7732cda5cfe52fd
radhikadhanji <radhikadhanji0517@gmail.com>
2023-09-07 10:08:42 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -21,27 +21,27 @@ function setup_layers(pScope){
   layer2.set_boundary( 0, 600 );
 }
 
-// function faces(x, y, animation, pScope){
+function faces(x, y, animation, pScope){
   
-//   //scale(animation.frame*2); //animation.frame*2
+  //scale(animation.frame*2); //animation.frame*2
 
-//   ellipse(0,0,50,50); // draw head
-//   fill(30);
-//   ellipse(-10,-10,10,10); //draw eye
-//   ellipse(10,-10,10,10); // draw eye
-//   arc(0,10,20,10,0,180); // draw mouth
+  ellipse(0,0,50,50); // draw head
+  fill(30);
+  ellipse(-10,-10,10,10); //draw eye
+  ellipse(10,-10,10,10); // draw eye
+  arc(0,10,20,10,0,180); // draw mouth
 
-//   let angleOffset = (360 / SLICE_COUNT) / 2
-//   let backgroundArcStart = 270 - angleOffset;
-//   let backgroundArcEnd = 270 + angleOffset;
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 270 - angleOffset;
+  let backgroundArcEnd = 270 + angleOffset;
 
-//   fill(66, 135, 245)
-//   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  fill(66, 135, 245)
+  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-//   fill(255)
-//   rect(-10,-800-animation.wave()*50,60, 60) // .wave is a cosine wave btw
+  fill(255)
+  rect(-10,-800-animation.wave()*50,60, 60) // .wave is a cosine wave btw
 
-// }
+}
 
 function squares(x, y, animation, pScope){
 
