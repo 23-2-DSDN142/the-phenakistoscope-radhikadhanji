e3e22b91a420116854e982a8b7623516921084bb
radhikadhanji <radhikadhanji0517@gmail.com>
2023-09-11 10:07:59 +1200

    11 september

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,10 +1,10 @@
-const SLICE_COUNT = 14; 
+const SLICE_COUNT = 12; //14
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false); //set back to true
-  pScope.set_direction(CCW);
+  pScope.set_direction(CW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image("whale" , "png");
 }
@@ -13,58 +13,63 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
+  var layer1 = new PLayer(clouds);
   layer1.mode( RING );
   layer1.set_boundary( 0, 1000 ); //1000
 
   var layer2 = new PLayer(whales);
   layer2.mode( RING );
-  layer2.set_boundary( 0, 600 );
+  layer2.set_boundary( 0, 1000 ); //800
 }
 
-function faces(x, y, animation, pScope){
-  
-  //scale(animation.frame*2); //animation.frame*2
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+function clouds(x, y, animation, pScope){
 
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
+  
+  let first = color(219, 251, 255);
+  let last = color(140, 176, 237);
+  let gradientCol = lerpColor(first, last, 0.5);
+
+  pScope.fill_background(gradientCol);
 
   fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  arc(x,y,900,900,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  //800 800
 
   fill(255)
-  rect(-10,-800-animation.wave()*50,60, 60) // .wave is a cosine wave btw
+  //Constructing the cloud
+  strokeWeight(4);
+  ellipse(-90,-785-animation.wave()*50, 100, 75); //left edge cloud
+  ellipse(70,-785-animation.wave()*50, 100, 75); //right edge cloud
+  ellipse(-10,-800-animation.wave()*50,150, 100); //bottom of cloud
+  noStroke();
+  ellipse(40,-850-animation.wave()*50, 100, 100);
+  ellipse(-50,-825-animation.wave()*50, 100, 100);
+
+  //Cloud shading
+  fill(250, 225, 202);
+  ellipse(-90,-785-animation.wave()*50, 100, 75); //left edge cloud
+  ellipse(70,-785-animation.wave()*50, 100, 75); //right edge cloud
+  ellipse(-10,-789-animation.wave()*50,150, 75); //bottom of cloud
 
 }
 
 function whales(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
- 
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
   //fill(66, 135, 245)
   //arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
   push()
-  scale(3)
+  //scale(3)
   if(animation.frame == 0){
-    fill(66, 135, 245)
-    arc(x,y,270,270,backgroundArcStart,backgroundArcEnd);
+    fill(255)
+    scale(0.3);
+   pScope.draw_image("whale", -10,-1500-animation.wave()*100); //x, y
   }
   pop()
-
-  fill(255)
-  pScope.draw_image("whale", x,y);
   //rect(-10,-450-animation.wave()*50,60, 60) // .wave is a cosine wave btw
 //20 20
 //-300
